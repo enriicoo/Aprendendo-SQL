6dece905dfc3cc09bcbe7e02c584198f3f76d057
Enrico Costa <69048124+enriicoo@users.noreply.github.com>
2022-03-28 18:47:59 -0300

    Initializing Repository

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO regions(name)
+VALUES ('Kanto'), ('Johto'), ('Hoenn');
+
+INSERT INTO pokedex (name, type, region)
+VALUES ('Bulbasaur', 'grass', 1), ('Venosaur', 'grass', 1), 
+('Charmander', 'fire', 1), ('Charizard', 'fire', 1), 
+('Squirtle', 'water', 1), ('Blastoise', 'water', 1),
+('Totodile', 'water', 2), ('Feraligatr', 'water', 2);
