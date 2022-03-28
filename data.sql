1bd51e6da9bb4a9f1c445afca7daa27718029903
Enrico Costa <69048124+enriicoo@users.noreply.github.com>
2022-03-28 20:24:32 -0300

    Valores das tabelas básicas

--- data.sql
+++ data.sql
@@ -1,8 +1,15 @@
 INSERT INTO regions(name)
 VALUES ('Kanto'), ('Johto'), ('Hoenn');
 
-INSERT INTO pokedex (name, type, region)
-VALUES ('Bulbasaur', 'grass', 1), ('Venosaur', 'grass', 1), 
-('Charmander', 'fire', 1), ('Charizard', 'fire', 1), 
-('Squirtle', 'water', 1), ('Blastoise', 'water', 1),
-('Totodile', 'water', 2), ('Feraligatr', 'water', 2);
+INSERT INTO pokedex (name, type, evolution, region_id)
+VALUES ('Bulbasaur', 'grass', 1, 1), ('Venusaur', 'grass', 2, 1), 
+('Charmander', 'fire', 1, 1), ('Charizard', 'fire', 2, 1), 
+('Squirtle', 'water', 1, 1), ('Blastoise', 'water', 2, 1),
+
+('Chikorita', 'grass', 1, 2), ('Meganium', 'grass', 2, 2), 
+('Cyndaquil', 'fire', 1, 2)('Typhlosion', 'fire', 2, 2)
+('Totodile', 'water', 1, 2), ('Feraligatr', 'water', 2, 2)
+
+('Treecko', 'grass', 1, 3), ('Sceptile', 'grass', 2, 3), 
+('Torchic', 'fire', 1, 3)('Blaziken', 'fire', 2, 3)
+('Mudkip', 'water', 1, 3), ('Swampert', 'water', 2, 3)
